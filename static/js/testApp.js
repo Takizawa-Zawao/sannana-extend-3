1d6252d0366e741c84754940ed32c8a697134eff
Takizawa-Zawao <106427546+Takizawa-Zawao@users.noreply.github.com>
2022-06-23 21:43:04 +0900

    ok

--- static/js/testApp.js
+++
@@ -1,9 +0,0 @@
-export default class TestApp extends React.Component{
-    render(){
-        return <h1>OK</h1>;
-    }
-}
-
-const app = document.getElementById("app");
-const root = ReactDOM.createRoot(app);
-root.render(<TestApp />);
