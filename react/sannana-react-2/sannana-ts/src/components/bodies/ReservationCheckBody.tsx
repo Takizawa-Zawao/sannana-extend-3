51754e56b2dff2200bacf95d4463b9f0d51fc0f9
Takizawa-Zawao <interlock.takizawa@gmail.com>
2022-06-26 14:19:54 +0900

    ok

--- react/sannana-react-2/sannana-ts/src/components/bodies/ReservationCheckBody.tsx
+++ react/sannana-react-2/sannana-ts/src/components/bodies/ReservationCheckBody.tsx
@@ -42,7 +42,7 @@ class ReservationCheckBody extends React.Component<Props, State>{
             additionalForm01: this.createAdditionalForm("mail_form"),
             additionalForm02: <div></div>
         });
-        alert(this.state.additionalForm01?.toString);
+        alert(this.state.additionalForm01?.toLocaleString());
         return;
     }
 
@@ -51,7 +51,7 @@ class ReservationCheckBody extends React.Component<Props, State>{
             additionalForm01: <div></div>,
             additionalForm02: this.createAdditionalForm("id_form")
         });
-        alert(this.state.additionalForm02?.toString);
+        alert(this.state.additionalForm02?.toLocaleString());
         return;
     }
 
