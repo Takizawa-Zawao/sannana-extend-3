728e78f1201da11596012750d857deb78dd725ed
Takizawa-Zawao <interlock.takizawa@gmail.com>
2022-06-27 21:40:37 +0900

    ok

---
+++ react/sannana-react-2/sannana-ts/src/components/bodies/AdditionalGuest.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type Props = {
+
+}
+
+type State = {
+
+}
+
+class AdditionalGuest extends React.Component<Props, State>{
+    constructor(props: Props){
+        super(props);
+    }
+
+    render(): React.ReactNode {
+        return(
+
+        );
+    }
+}
+
+export default AdditionalGuest;
