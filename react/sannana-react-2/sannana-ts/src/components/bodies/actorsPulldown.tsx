8333c49c565c7b3f62e8af851e2b41a417d56873
Takizawa-Zawao <interlock.takizawa@gmail.com>
2022-07-17 19:37:17 +0900

    ok

--- react/sannana-react-2/sannana-ts/src/components/bodies/actorsPulldown.tsx
+++ react/sannana-react-2/sannana-ts/src/components/bodies/actorsPulldown.tsx
@@ -12,18 +12,20 @@ type State = {
 class actorsPulldown extends React.Component<Props, State>{
     render(): React.ReactNode {
         let options: Array<React.ReactNode> = [];
-        () => {
-            for (let actor_id in this.props.actorsList.keys()) {
-                let option_tmp = <option value={actor_id}>{this.props.actorsList.get(parseInt(actor_id))}</option>
-                options.push(option_tmp)
-            }
-        }
+        this.createOptions(options);
         return (
             <select name="invitation_id_0">
                 {options}
             </select>
         )
     }
+
+    createOptions(options: React.ReactNode[]){
+        for (let actor_id in this.props.actorsList.keys()) {
+            let option_tmp = <option value={actor_id}>{this.props.actorsList.get(parseInt(actor_id))}</option>
+            options.push(option_tmp)
+        }
+    } 
 }
 
 export default actorsPulldown;
